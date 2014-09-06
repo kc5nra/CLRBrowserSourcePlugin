57665459ad4d5f076e0c6f13799af9a4ec3d1f69
John R. Bradley <jrb@turrettech.com>
2014-09-05 23:18:04 -0500

    Fix how switches are added to command line

--- Plugin/Shared/BrowserApp.cs
+++ Plugin/Shared/BrowserApp.cs
@@ -24,12 +24,42 @@ namespace CLRBrowserSourcePlugin.Shared
 
         protected override void OnBeforeCommandLineProcessing(string processType, CefCommandLine commandLine)
         {
+
             // only happens if configuration is bad
             if (arguments != null)
             {
                 foreach (String argument in arguments)
                 {
-                    commandLine.AppendSwitch(argument);
+                    string normalizedArgument = argument;
+                    if (argument.StartsWith("--"))
+                    {
+                        if (argument.Length > 2) {
+                            normalizedArgument = argument.Substring(2);
+                        } else {
+                            Console.WriteLine(
+                                "BrowserApp::OnBeforeCommandLineProcessing bad argument {0}",
+                                argument);
+                            continue;
+                        }
+                    }
+
+                    int argSplitIndex = normalizedArgument.IndexOf('=');
+                    if (argSplitIndex >= 0)
+                    {
+                        string name = normalizedArgument.Substring(0, argSplitIndex);
+                        string value = normalizedArgument.Substring(argSplitIndex + 1);
+                        
+                        if (value.StartsWith("\"") && value.EndsWith("\""))
+                        {
+                            value = value.Substring(1, value.Length - 2);
+                        }
+                        
+                        commandLine.AppendSwitch(name, value);
+                    }
+                    else
+                    {
+                        commandLine.AppendSwitch(normalizedArgument);
+                    }
                 }
             }
             
